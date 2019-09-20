3562c08600deb82cad17b907cacf47f7b69e97fc
Alexius Academia <alexius.academia@gmail.com>
2019-09-21 06:17:49 +0800

    MPa to PSI

--- pyrca/utils/conversion.py
+++ pyrca/utils/conversion.py
@@ -1,13 +1,21 @@
 import math
+from .constants import *
 
 
 def psi_to_mpa(psi):
     """
     Converts pressure from PSI to MPa.
     :param psi: The pressure in PSI
-    :return: float Pressure in MPa.
+    :return: Pressure in MPa.
     """
-    psi = psi / 2.204
-    psi = psi * 9.81
+    psi = psi / POUND_PER_KILOGRAM
+    psi = psi * GRAVITY_METRIC
     psi = psi / math.pow(25.4, 2)
     return psi
+
+
+def mpa_to_psi(mpa):
+    mpa = mpa * POUND_PER_KILOGRAM
+    mpa = mpa / GRAVITY_METRIC
+    mpa = mpa * math.pow(25.4, 2)
+    return mpa
