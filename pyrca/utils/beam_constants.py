5a7a69c5a04346aa417a521ed14cc0d3cbb14f20
Alexius Academia <alexius.academia@gmail.com>
2019-09-21 07:51:32 +0800

    Beam constants

---
+++ pyrca/utils/beam_constants.py
@@ -0,0 +1,13 @@
+# Modulus of elasticity of steel
+# Unit in MPa
+ES = 200000
+
+# Maximum strain in concrete
+MAX_CONCRETE_STRAIN = 0.003
+
+# Concrete compressive strength threshold, before
+# changing Beta1
+COMPRESSIVE_STRENGTH_THRESHOLD = 30
+
+# Iteration for the compression area integration
+COMPRESSION_SOLID_DY_ITERATION = 1000
