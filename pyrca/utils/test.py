ef4b8bbb25a3ecf51e68fc2fc52fd417b36575c7
Alexius Academia <alexius.academia@gmail.com>
2019-09-21 06:21:18 +0800

    Test for PSI to MPa conversion

--- pyrca/utils/test.py
+++ pyrca/utils/test.py
@@ -4,3 +4,8 @@ from pyrca.utils.conversion import *
 mpa = 10
 psi = mpa_to_psi(mpa)
 print(str(mpa) + ' MPa to PSI = ' + str(psi))
+
+# Test the PSI to MPa conversion
+psi = 10
+mpa = psi_to_mpa(psi)
+print(str(psi) + ' PSI to MPa = ' + str(mpa))
