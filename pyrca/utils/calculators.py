48dd11113af9458fea0bdc2f7779f13686419a92
Alexius Academia <alexius.academia@gmail.com>
2019-09-21 09:19:56 +0800

    Calculate distance of centroid from top

--- pyrca/utils/calculators.py
+++ pyrca/utils/calculators.py
@@ -26,3 +26,49 @@ def calculate_area(nodes: list):
 
     _area = abs(_area) / 2
     return _area
+
+
+def calculate_centroid_y(nodes: list):
+    """
+    Calculates centroid of a polygon from the top.
+    :param nodes: Nodes list. The coordinates of each corner of a section.
+    :return: Distance of centroid from the top.
+    """
+    _nodes = nodes.copy()
+
+    # Add the last node to the list
+    _nodes.append(_nodes[-1])
+
+    # Number of nodes
+    _n = len(_nodes)
+
+    _area = calculate_area(_nodes)
+
+    _kd = 0
+
+    for i in range(_n - 1):
+        _kd += (_nodes[i].y + _nodes[i+1].y) * \
+              (_nodes[i].x * _nodes[i+1].y - _nodes[i+1].x * _nodes[i].y)
+
+    _kd = abs(_kd / (6 * _area))
+    _kd = abs(get_highest_node(_nodes).y - _kd)
+
+    return _kd
+
+
+def get_highest_node(nodes: list):
+    """
+    Get the topmost node from the section.
+    :param nodes: Nodes list. The coordinates of each corner of a section.
+    :return: Highest node.
+    """
+    if len(nodes) == 0:
+        return None
+
+    _highest_node = nodes[0]
+
+    for _node in nodes:
+        if _node.y > _highest_node.y:
+            _highest_node = _node
+
+    return _highest_node
--- pyrca/utils/test_calculators.py
+++ pyrca/utils/test_calculators.py
@@ -1,6 +1,6 @@
 import unittest
 from ..properties.node import Node
-from .calculators import calculate_area
+from .calculators import calculate_area, calculate_centroid_y
 
 
 class TestCalculators(unittest.TestCase):
@@ -8,6 +8,11 @@ class TestCalculators(unittest.TestCase):
         data = [Node(0, 0), Node(0, 400), Node(200, 400), Node(200, 0)]
         result = calculate_area(data)
         self.assertEqual(result, 80000, msg='Error in calculation of area!')
+
+    def test_centroid_y(self):
+        data = [Node(0, 0), Node(0, 400), Node(200, 400), Node(200, 0)]
+        result = calculate_centroid_y(data)
+        self.assertEqual(result, 200, msg='Error in calculation of centroid y.')
 
 
 if __name__ == '__main__':
