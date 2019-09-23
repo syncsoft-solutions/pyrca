79f2389ca60a7213a839c23b508d9788dc2f3be6
Alexius Academia <alexius.academia@gmail.com>
2019-09-23 20:05:22 +0800

    Fixed test

--- pyrca/analysis/test/test_beam_analysis.py
+++ pyrca/analysis/test/test_beam_analysis.py
@@ -10,21 +10,28 @@ from ..stress_distribution import StressDistribution
 class TestBeamAnalysis(unittest.TestCase):
     _nodes = [Node(0, 0), Node(0, 500), Node(250, 500), Node(250, 0)]
 
-    _section = Section()
-    _section.set_main_section(_nodes)
+    def test_beam_capacity(self):
+        _section = Section()
+        _section.set_main_section(self._nodes)
 
-    _bs = BeamSection()
-    _bs.set_fc_prime(20.7)
-    _bs.set_fy(276.5)
-    _bs.set_effective_depth(460)
+        _bs = BeamSection()
+        _bs.set_fc_prime(20.7)
+        _bs.set_fy(276.5)
+        _bs.set_effective_depth(460)
+        _bs.section = _section
 
-    _tension_steel = SteelTension()
-    _tension_steel.set_total_area(1472.62)
+        _tension_steel = SteelTension()
+        _tension_steel.set_total_area(1472.62)
 
-    _bs.steel_tension = _tension_steel
+        _bs.steel_tension = _tension_steel
 
-    _analysis = BeamAnalyses.beam_capacity_analysis(sd=StressDistribution.WHITNEY)
-    print(_analysis)
+        _analysis = BeamAnalyses()
+        _analysis.beam_section = _bs
+        _result = _analysis.beam_capacity_analysis(StressDistribution.WHITNEY)
+
+        _mu = _result.moment_c * 0.9
+
+        self.assertAlmostEqual(_mu, 151.56, 2, msg='Wrong calculation!')
 
 
 if __name__ == '__main__':
