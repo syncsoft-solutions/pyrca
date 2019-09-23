3bbec8b31a1251d6a37a9eaf8a4d64ab8f0de53c
Alexius Academia <alexius.academia@gmail.com>
2019-09-24 07:46:09 +0800

    Comments for the unit testing

--- pyrca/analysis/test/test_beam_analysis.py
+++ pyrca/analysis/test/test_beam_analysis.py
@@ -8,6 +8,7 @@ from ..stress_distribution import StressDistribution
 
 
 class TestBeamAnalysis(unittest.TestCase):
+    # Definition of beam section nodes.
     _nodes = [Node(0, 0), Node(0, 500), Node(250, 500), Node(250, 0)]
 
     def test_beam_capacity(self):
@@ -22,26 +23,36 @@ class TestBeamAnalysis(unittest.TestCase):
         From solution on the book, Mu = 151.56 kN-m
         :return:
         """
+
+        # Section object
         _section = Section()
         _section.set_main_section(self._nodes)
 
+        # Beam section object
         _bs = BeamSection()
         _bs.set_fc_prime(20.7)
         _bs.set_fy(276.5)
         _bs.set_effective_depth(460)
         _bs.section = _section
 
+        # Tensile reinforcement object
         _tension_steel = SteelTension()
         _tension_steel.set_total_area(1472.62)
 
+        # Set the beam's tensile reinforcement
         _bs.steel_tension = _tension_steel
 
+        # Define analyses object to be able to use the different analysis (e.g. uncrack, capacity/nominal)
         _analysis = BeamAnalyses()
         _analysis.beam_section = _bs
+
+        # Analysis result
         _result = _analysis.beam_capacity_analysis(StressDistribution.WHITNEY)
 
+        # Converted from nominal to suit the requirement from the problem.
         _mu = round(_result.moment_c * 0.9, 2) / 1000000
 
+        # Checking the calculation against the manual method.
         self.assertAlmostEqual(_mu, 151.56, 0, msg='Wrong calculation!')
 
 
