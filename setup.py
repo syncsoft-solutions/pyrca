4a2f95f469dc06b577fdfb2d67b6fe3abf73202f
Alexius Academia <alexius.academia@gmail.com>
2019-09-25 22:59:39 +0800

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 import setuptools
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="pyrca",
-    version="0.0.3",
+    version="0.2.1",
     author="Alexius Sayco Academia",
     author_email="alexius.academia@gmail.com",
     description="Python package for analysis of concrete structures.",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/syncsoft-solutions/pyrca",
     packages=setuptools.find_packages(),
